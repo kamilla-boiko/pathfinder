df9864cff0e6a8cfc2c5623df28f57e0cb61c823
kruzhanska <57404817+kruzhanska@users.noreply.github.com>
2019-12-13 22:45:43 +0200

    Update mx_memmove.c

--- libmx/src/mx_memmove.c
+++ libmx/src/mx_memmove.c
@@ -4,10 +4,11 @@ void *mx_memmove(void *dst, const void *src, size_t len) {
     char *d = dst;
     const char *s = src;
     char tmp[len];
+    size_t i = 0;
 
-    for (size_t i = 0; i < len; i++)
+    for (i = 0; i < len; i++)
         tmp[i] = s[i];
-    for (size_t i = 0; i < len; i++)
+    for (i = 0; i < len; i++)
         d[i] = tmp[i];
     return dst;
 }
