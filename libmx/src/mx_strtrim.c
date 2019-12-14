f4f692ad8050de136ec8eac3c31c36c1b0d8fe9a
kruzhanska <57404817+kruzhanska@users.noreply.github.com>
2019-12-14 14:49:45 +0200

    Update mx_strtrim.c

--- libmx/src/mx_strtrim.c
+++ libmx/src/mx_strtrim.c
@@ -1,20 +1,15 @@
 #include "libmx.h"
 
 char *mx_strtrim(const char *str) {
-    int start = 0;
-    int end = 0;
     int i = 0;
     int j = 0;
     char *trim = NULL;
 
     if (str == NULL)
         return NULL;
-    end = mx_strlen(str);
     for (; mx_isspace(str[i]) == 1; i++);
-    start = i;
-    j = end - 1;
+    j = mx_strlen(str) - 1;
     for (; mx_isspace(str[j]) == 1; j--);
-    end = j;
     trim = mx_strnew(j - i + 1);
     for (int k = 0; i <= j; k++, i++)
         trim[k] = str[i];
