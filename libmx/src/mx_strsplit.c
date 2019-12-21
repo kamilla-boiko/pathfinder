1a2f239efa9836ab6a5aef080010c6a37160f687
kruzhanska <57404817+kruzhanska@users.noreply.github.com>
2019-12-21 15:37:29 +0200

    Add files via upload

--- libmx/src/mx_strsplit.c
+++ libmx/src/mx_strsplit.c
@@ -3,14 +3,13 @@
 char **mx_strsplit(const char *s, char c) {
     int size_arr = mx_count_words(s, c);
     char **arr = malloc((size_arr + 1) * sizeof(char *));
-    int size_str = mx_strlen_my(s, c);
 
     if (s == NULL)
         return NULL;
     for (int i = 0; i < size_arr; i++) {
         while (*s == c)
             s++;
-        arr[i] = mx_strnew(size_str);
+        arr[i] = mx_strnew(mx_strlen_my(s, c));
         for (int j = 0; *s != c; j++) {
             arr[i][j] = *s;
             s++;
